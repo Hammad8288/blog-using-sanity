4b21e1628a4c688c52133fb7cd566163adad0685
Hammad8288 <159915404+Hammad8288@users.noreply.github.com>
2025-01-03 22:39:10 +0500

    commit

--- src/app/BlogPost/[slug]/page.tsx
+++ src/app/BlogPost/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { client } from "@/sanity/lib/client";
 import React from "react";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 interface BlogPostProps {
   params: {
@@ -9,7 +10,7 @@ interface BlogPostProps {
 }
 
 const BlogPost = async ({ params }: BlogPostProps) => {
-  const { slug } = params;
+  const { slug } = await params;
 
   const query = `*[_type == "blog" && slug.current == $slug][0] {
     title,
@@ -28,18 +29,22 @@ const BlogPost = async ({ params }: BlogPostProps) => {
   return (
     <div className="container mx-auto max-w-3xl px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1> 
-      <img
+      <Image
         src={post.imageUrl}
         alt={post.title}
+        width={800}
+        height={400}
         className="w-full max-h-96 object-cover rounded-lg mb-6"
       />
       {post.author && (
         <div className="mt-8">
           <h3 className="text-lg font-semibold">Author</h3>
           <div className="flex items-center mt-2">
-            <img
+            <Image
               src={post.author.authorimage}
               alt={post.author.authorname}
+              width={48}
+              height={48}
               className="w-12 h-12 rounded-full mr-4"
             />
             <p className="font-bold underline">{post.author.authorname}</p>
